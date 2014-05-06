de1f49d385a7c769c40f01c38884788a2eb8d10f
aniversarioperu <aniversarioperu1@gmail.com>
2014-05-06 23:59:46 +0300

    calculate rates

--- analisis.R
+++ analisis.R
@@ -5,3 +5,31 @@ names(datos) <- c("2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011"
 # traspose matrix
 datos_t <- as.data.frame(t(datos))
 datos <- data.frame(Año = row.names(datos_t), datos_t)
+
+# Calcular disminución de la pobreza en porcentajes
+a2005 <- datos[2,] - datos[1,]
+a2005[1,1] <- 2005
+
+a2006 <- datos[3,] - datos[2,]
+a2006[1,1] <- 2006
+
+a2007 <- datos[4,] - datos[3,]
+a2007[1,1] <- 2007
+
+a2008 <- datos[5,] - datos[4,]
+a2008[1,1] <- 2008
+
+a2009 <- datos[6,] - datos[5,]
+a2009[1,1] <- 2009
+
+a2010 <- datos[7,] - datos[6,]
+a2010[1,1] <- 2010
+
+a2011 <- datos[8,] - datos[7,]
+a2011[1,1] <- 2011
+
+a2012 <- datos[9,] - datos[8,]
+a2012[1,1] <- 2012
+
+a2013 <- datos[10,] - datos[9,]
+a2013[1,1] <- 2013
